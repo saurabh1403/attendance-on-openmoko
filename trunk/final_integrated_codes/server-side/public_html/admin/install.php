5e68a6a17888aa5fdb79b2c60a4d7069ec74cca6
saurabhgupta1403 <saurabhgupta1403@921b6ea8-b952-11dd-a503-ed2d4bea8bb5>
2009-05-06 16:27:58 +0000

    working code here. previous version was not complete. safe point here

--- trunk/final_integrated_codes/server-side/public_html/admin/install.php
+++ trunk/final_integrated_codes/server-side/public_html/admin/install.php
@@ -1,61 +1,54 @@
-
-
 <?php
 
 //load the whole schema and create the database and update it with initial names
+require_once '../lib-common1.php';
 
-require_once '../lib-common.php';
-require_once $_CONF['path'] . 'db.php';
-require_once $_CONF['path'] . 'schema_database.php';
+require_once $_CONF1['path'] . 'db.php';
+require_once $_CONF1['path'] . 'schema_database.php';
 
 
 function install_DASO()
 {
-	global $_DB,$_CONF, $_FILE_NAMES, $_SCHEMA;
+	global $_DB1,$_CONF1, $_FILE_NAMES, $_SCHEMA;
 
 	//connecting to mysql database
 	connect_database($con);
-	mysql_select_db($_DB['name'], $con);
+	mysql_select_db($_DB1['name'], $con);
 
 	//***********************creating table ODASI************************
-	$sql_query=	"DROP table " . $_DB['table_prefix'] . "ODASI";	
-	execute_query_NR($_DB['name'],$con,$sql_query);	
+	$sql_query=	"DROP table " . $_DB1['table_prefix'] . "ODASI";	
+	execute_query_NR($_DB1['name'],$con,$sql_query);	
 
-	$sql_query = "CREATE TABLE " . $_DB['table_prefix'] . "ODASI(" . $_SCHEMA['ODASI'];	
-	execute_query_NR($_DB['name'],$con,$sql_query);	
-
+	$sql_query = "CREATE TABLE " . $_DB1['table_prefix'] . "ODASI(" . $_SCHEMA['ODASI'];	
+	execute_query_NR($_DB1['name'],$con,$sql_query);	
 
 	//********************updating table ODASI and creating tables for names, attendance and notes***************************
-	$class_list = $_CONF['DataDir'] . $_FILE_NAMES['class_list'];
+	$class_list = $_CONF1['DataDir'] . $_FILE_NAMES['class_list'];
 	file_read_linewise($class_list, $class_names, $no_items);
 
 	for($i = 0; $i < $no_items; $i++)
 	{
 		list($info['Branch'],$info['SubjectCode'],$info['Section'],$info['Year']) = split('_',$class_names[$i]);
-		$info['name_file']  =  $_DB['table_prefix'] . "names_" . $class_names[$i];
-		$info['attendance_file']  =  $_DB['table_prefix'] . "att_" . $class_names[$i];
-		$info['notes_file']  =  $_DB['table_prefix'] . "notes_" . $class_names[$i];
-		$sql_query  = "INSERT INTO " . $_DB['table_prefix']. "ODASI (Branch, Section, Year, name_file, attendance_file, notes_file) VALUES('". $info['Branch']. "', '" . $info['Section']. "', '" . $info['Year']. "', '" . $info['name_file']. "', '" . $info['attendance_file']. "', '" . $info['notes_file']. "')";
-		execute_query_NR($_DB['name'],$con,$sql_query);	
+		$info['name_file']  =  $_DB1['table_prefix'] . "names_" . $class_names[$i];
+		$info['attendance_file']  =  $_DB1['table_prefix'] . "att_" . $class_names[$i];
+		$info['notes_file']  =  $_DB1['table_prefix'] . "notes_" . $class_names[$i];
+		$sql_query  = "INSERT INTO " . $_DB1['table_prefix']. "ODASI (Branch, Section, Year, name_file, attendance_file, notes_file) VALUES('". $info['Branch']. "', '" . $info['Section']. "', '" . $info['Year']. "', '" . $info['name_file']. "', '" . $info['attendance_file']. "', '" . $info['notes_file']. "')";
+		execute_query_NR($_DB1['name'],$con,$sql_query);	
 
-		create_class_names($_DB['name'],$con, $info['name_file']);
+		create_class_names($_DB1['name'],$con, $info['name_file']);
 
-		$class_names_file = $_CONF['DataDir'] . $class_names[$i].".txt";
+		$class_names_file = $_CONF1['DataDir'] . $class_names[$i].".txt";
 		file_read_linewise($class_names_file, $names,$no_students);
 		$roll_no = range(1,$no_students);
-		update_class_names($_DB['name'],$con, $info['name_file'], $roll_no, $names);
+		update_class_names($_DB1['name'],$con, $info['name_file'], $roll_no, $names);
 		
-		create_attendance_table($_DB['name'],$con,$info['attendance_file'],$info['name_file']);
-		create_notes_table($_DB['name'],$con,$info['notes_file'],$info['name_file']);
+		create_attendance_table($_DB1['name'],$con,$info['attendance_file'],$info['name_file']);
+		create_notes_table($_DB1['name'],$con,$info['notes_file'],$info['name_file']);
 
 	}
 
-
 }
 
-
 install_DASO();
-
-
 
 ?>
